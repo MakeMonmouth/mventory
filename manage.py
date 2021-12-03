c04745e227d43c2c1a20341ee2de96e534caa052
Matthew Macdonald-Wallace <matt@doics.co>
2021-12-03 06:04:59 +0000

    Move OTLP setup to wsgi.py

--- manage.py
+++ manage.py
@@ -3,42 +3,9 @@
 import os
 import sys
 
-from opentelemetry.instrumentation.django import DjangoInstrumentor
-from opentelemetry.instrumentation.logging import LoggingInstrumentor
-from opentelemetry import trace
-from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
-from opentelemetry.sdk.resources import Resource
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import (
-    BatchSpanProcessor,
-    ConsoleSpanExporter,
-    )
-from opentelemetry.propagate import set_global_textmap
-from opentelemetry.propagators.b3 import B3MultiFormat
-
-
-
 def main():
-    set_global_textmap(B3MultiFormat())
-    resource = Resource(attributes={
-        "service.name": "mventory",
-        "environment": "dev"
-    })
-    
-    trace.set_tracer_provider(TracerProvider(resource=resource))
-    tracer = trace.get_tracer(__name__)
-    
-    otlp_exporter = OTLPSpanExporter(endpoint="https://otlphttp.service.wallace.network/v1/traces")
-    
-    span_processor = BatchSpanProcessor(otlp_exporter)
-    
-    trace.get_tracer_provider().add_span_processor(span_processor)
-
-    DjangoInstrumentor().instrument()
-    """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mventory.settings')
     os.environ.setdefault("OPENTELEMETRY_PYTHON_DJANGO_INSTRUMENT", "True")
-    LoggingInstrumentor().instrument(set_logging_format=True)
 
     try:
         from django.core.management import execute_from_command_line
--- mventory/wsgi.py
+++ mventory/wsgi.py
@@ -8,10 +8,46 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
+import socket
 
 from opentelemetry.instrumentation.wsgi import OpenTelemetryMiddleware
+from opentelemetry.instrumentation.django import DjangoInstrumentor
+from opentelemetry.instrumentation.logging import LoggingInstrumentor
+from opentelemetry.instrumentation.mysql import MySQLInstrumentor
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import (
+    BatchSpanProcessor,
+    ConsoleSpanExporter,
+    )
+resource = Resource(attributes={
+    "service.name": "mventory",
+    "environment": "dev",
+    "host": socket.gethostname()
+})
+
+trace.set_tracer_provider(TracerProvider(resource=resource))
+tracer = trace.get_tracer(__name__)
+
+otlp_exporter = OTLPSpanExporter(endpoint=f"{os.getenv('OTEL_EXPORTER_OTLP_ENDPOINT')}/v1/traces")
+
+span_processor = BatchSpanProcessor(otlp_exporter)
+
+trace.get_tracer_provider().add_span_processor(span_processor)
+
+def request_hook(span, request):
+    print(f"Request: {span.context}")
+
+def response_hook(span, request, response):
+    print(f"Response: {span.context}")
+
+
 
 from django.core.wsgi import get_wsgi_application
+DjangoInstrumentor().instrument(request_hook=request_hook, response_hook=response_hook)
+LoggingInstrumentor().instrument(set_logging_format=True)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mventory.settings')
 
