9d09a1373b13f28c224f413f5d0ab938b3013cef
Matthew Macdonald-Wallace <matt@doics.co>
2022-03-24 17:29:12 +0000

    Switch to Heroku-preferred location for static files

--- mventory/settings.py
+++ mventory/settings.py
@@ -137,10 +137,14 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = 'assets'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
