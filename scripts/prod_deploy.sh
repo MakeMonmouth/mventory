e94e8047f826508273c8288c00ac8cb9f1a3ff2f
Matthew Macdonald-Wallace <matt@doics.co>
2022-03-24 15:16:30 +0000

    Auto create the super-user on first run

--- scripts/prod_deploy.sh
+++ scripts/prod_deploy.sh
@@ -2,12 +2,23 @@
 
 set -eux
 
-env | grep MV
-
 python manage.py wait_for_db
 python manage.py collectstatic --noinput
 python manage.py migrate
 
-#unset MVENTORY_DEBUG
+script="
+from django.contrib.auth.models import User;
+
+username = '$MVENTORY_ADMIN_USER';
+password = '$MVENTORY_ADMIN_PASS';
+email = '$MVENTORY_ADMIN_EMAIL';
+
+if User.objects.filter(username=username).count()==0:
+    User.objects.create_superuser(username, email, password);
+    print('Superuser created.');
+else:
+    print('Superuser creation skipped.');
+"
+printf "$script" | python manage.py shell
 
 uwsgi --ini uwsgi.ini
